25bc1386fb4a419b14f7f209316baf4970f9e6df
Nguyễn Tiến Kha <khantse170170@fpt.edu.vn>
2025-03-03 01:00:33 +0700

    update input request - main flow

---
+++ src/routes/inputs/index.js
@@ -0,0 +1,167 @@
+const express = require("express");
+const { catchAsyncHandle } = require("../../middlewares/error.middleware");
+const AuthMiddleware = require("../../middlewares/auth.middleware");
+const inputController = require("../../controllers/input.controller");
+const checkRoles = require("../../middlewares/role.middleware");
+const { USER_ROLES } = require("../../configs/user.config");
+
+const router = express.Router();
+
+router.use(catchAsyncHandle(AuthMiddleware));
+
+router.get("/details",
+    /**
+      * #swagger.tags = ['Input']
+      * #swagger.description='Get all input details'
+      */
+    /* #swagger.parameters['query'] = {
+        in: 'query',
+        schema: {
+            $ref: "#/components/schemas/GetAllInputDetails"
+        }
+    } */
+    checkRoles({ requiredRoles: [USER_ROLES.MANAGER, USER_ROLES.INVENTORY_STAFF] }),
+    catchAsyncHandle(inputController.getAllInputDetails)
+);
+
+router.get("/details/:id",
+    /**
+      * #swagger.tags = ['Input']
+      * #swagger.description='Get input detail'
+      */
+    /* #swagger.parameters['id'] = {
+        in: 'path',
+        required: true,
+        description: 'Input detail id',
+        type: 'string'
+    } */
+    checkRoles({ requiredRoles: [USER_ROLES.MANAGER, USER_ROLES.INVENTORY_STAFF] }),
+    catchAsyncHandle(inputController.getInputDetail)
+);
+
+router.get("/",
+    /**
+      * #swagger.tags = ['Input']
+      * #swagger.description='Get all input requests'
+      */
+    /* #swagger.parameters['query'] = {
+        in: 'query',
+        schema: {
+            $ref: "#/components/schemas/GetAllInputRequests"
+        }
+    } */
+    catchAsyncHandle(inputController.getAllInputRequests)
+);
+
+router.get("/:id",
+    /**
+      * #swagger.tags = ['Input']
+      * #swagger.description='Get input request'
+      */
+    /* #swagger.parameters['id'] = {
+        in: 'path',
+        required: true,
+        description: 'Input request id',
+        type: 'string'
+    } */
+    checkRoles({ requiredRoles: [USER_ROLES.MANAGER, USER_ROLES.INVENTORY_STAFF, USER_ROLES.SUPPLIER] }),
+    catchAsyncHandle(inputController.getInputRequest)
+);
+
+router.post("/",
+    /**
+      * #swagger.tags = ['Input']
+      * #swagger.description='Create a new input request'
+      */
+    /*  #swagger.requestBody = {
+        content: {
+            "application/json": {
+                schema: {
+                    $ref: "#/components/schemas/CreateInputRequest"
+                }  
+            }
+        }
+    } */
+    checkRoles({ requiredRoles: [USER_ROLES.REPORT_STAFF] }),
+    catchAsyncHandle(inputController.createInputRequest)
+);
+
+router.patch("/:id/approve",
+    /**
+      * #swagger.tags = ['Input']
+      * #swagger.description='Approve input request'
+      */
+    /* #swagger.parameters['id'] = {
+        in: 'path',
+        required: true,
+        description: 'Input request id',
+        type: 'string'
+    } */
+    /*  #swagger.requestBody = {
+        content: {
+            "application/json": {
+                schema: {
+                    $ref: "#/components/schemas/ApproveInputRequest"
+                }  
+            }
+        }
+    } */
+    checkRoles({ requiredRoles: [USER_ROLES.MANAGER] }),
+    catchAsyncHandle(inputController.approveInputRequest)
+);
+
+router.patch("/:id/reject",
+    /**
+      * #swagger.tags = ['Input']
+      * #swagger.description='Reject input request'
+      */
+    /* #swagger.parameters['id'] = {
+        in: 'path',
+        required: true,
+        description: 'Input request id',
+        type: 'string'
+    } */
+    /*  #swagger.requestBody = {
+        content: {
+            "application/json": {
+                schema: {
+                    $ref: "#/components/schemas/RejectInputRequest"
+                }  
+            }
+        }
+    } */
+    checkRoles({ requiredRoles: [USER_ROLES.MANAGER] }),
+    catchAsyncHandle(inputController.rejectInputRequest)
+);
+
+router.patch("/:id/receive",
+    /**
+      * #swagger.tags = ['Input']
+      * #swagger.description='Receive input request'
+      */
+    /* #swagger.parameters['id'] = {
+        in: 'path',
+        required: true,
+        description: 'Input request id',
+        type: 'string'
+    } */
+    checkRoles({ requiredRoles: [USER_ROLES.INVENTORY_STAFF] }),
+    catchAsyncHandle(inputController.receiveInputRequest)
+);
+
+router.patch("/:id/cancel",
+    /**
+      * #swagger.tags = ['Input']
+      * #swagger.description='Cancel input request'
+      */
+    /* #swagger.parameters['id'] = {
+        in: 'path',
+        required: true,
+        description: 'Input request id',
+        type: 'string'
+    } */
+    checkRoles({ requiredRoles: [USER_ROLES.REPORT_STAFF, USER_ROLES.MANAGER] }),
+    catchAsyncHandle(inputController.cancelInputRequest)
+);
+
+module.exports = router;
